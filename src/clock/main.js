06f6120da163d87dd63dfad291b2483ed11a314c
jackice <275500122@qq.com>
2024-07-09 11:12:33 +0800

    feat: 调整样式,采用简单的风格只保留时钟

--- src/clock/main.js
+++ src/clock/main.js
@@ -1,53 +1,57 @@
 class MyClock extends HTMLElement {
-  static styles = `
+    static styles = `
 <style>
 
 :host {
   width: 100%;
   display: flex;
+  justify-content: center; /* 水平居中 */
+  align-items: center; /* 垂直居中 */
 }
 
 #clock {
     font-size: 5rem;
     font-weight: 700;
     letter-spacing: 10px;
+    font-family: "Open Sans", Arial, sans-serif;
     color: white;
+    // color: black;
   }
 
 @media screen and (min-width: 900px) {
   #clock {
-    font-size: 6rem;
+    font-size: 8rem;
   }
   }
 </style>
 `;
-  get template() {
-    return `
+    get template() {
+        return `
     ${MyClock.styles}
     <div id="clock"></div>
     `;
-  }
+    }
 
-  constructor() {
-    super();
-    this.attachShadow({ mode: "open" });
-  }
+    constructor() {
+        super();
+        this.attachShadow({ mode: "open" });
+    }
 
-  connectedCallback() {
-    this.shadowRoot.innerHTML = this.template;
-    this.init();
-  }
+    connectedCallback() {
+        this.shadowRoot.innerHTML = this.template;
+        this.init();
+    }
 
-  init() {
-    const setTime = () => {
-      const localeTimeString = new Date().toLocaleTimeString();
-      this.shadowRoot.getElementById("clock").innerHTML = localeTimeString;
-    };
-    setTime();
-    setInterval(() => {
-      setTime();
-    }, 1000);
-  }
+    init() {
+        const setTime = () => {
+            const localeTimeString = new Date().toLocaleTimeString();
+            this.shadowRoot.getElementById("clock").innerHTML = localeTimeString;
+        };
+        setTime();
+        setInterval(() => {
+            setTime();
+        }, 1000);
+    }
 }
 
 customElements.define("my-clock", MyClock);
